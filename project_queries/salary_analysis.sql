42949e9f588a70228590be78df282df51a9ccfcf
Visal Adithya <adithyavisal75@gmail.com>
2025-07-01 11:56:07 +0530

    push changes

--- project_queries/salary_analysis.sql
+++ project_queries/salary_analysis.sql
@@ -1,7 +1,7 @@
 -- 🎯 Objective 4: Calculate average salary by job title
 -- Handle NULLs by converting them to 0 before summing
 SELECT job_title_short,
-       ROUND(AVG(IFNULL(salary_year_avg, 0) + IFNULL(salary_hour_avg, 0) * 40 * 52), 2) AS average_total_salary
+       ROUND(AVG(IFNULL(salary_year_avg,0) + IFNULL(salary_hour_avg, 0) * 40 * 52), 0) AS average_total_salary
 FROM job_postings_fact
 GROUP BY job_title_short
 ORDER BY average_total_salary DESC;
@@ -11,7 +11,7 @@ ORDER BY average_total_salary DESC;
 -- 🎯 Objective 5: List top 10 companies by average yearly salary
 SELECT company_dim.name,
        ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_yearly_salary
-FROM job_postings_fact LEFT JOIN company_dim on job_postings_fact.company_id=company_dim.company_id
+FROM job_postings_fact LEFT JOIN company_dim ON job_postings_fact.company_id=company_dim.company_id
 GROUP BY company_dim.name
 ORDER BY avg_yearly_salary DESC
 LIMIT 10;
